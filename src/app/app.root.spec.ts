ce2ea0e00d4181f7bbf0ce112381a860e76af709
bmVsc29u <bmVsc29u@gmail.com>
2019-05-04 11:09:51 +0800

    spec: fix app.root.spec

--- src/app/app.root.spec.ts
+++ src/app/app.root.spec.ts
@@ -1,6 +1,13 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 import { TestBed, async } from '@angular/core/testing';
+import { of } from 'rxjs';
+
+const mockHttp = {
+  get() { return of('# test\ncontent\na'); }
+};
+
 import { AppRoot as Tag } from './app.root';
 
 describe(Tag.name, () => {
@@ -10,6 +17,9 @@ describe(Tag.name, () => {
       imports: [
         HttpClientTestingModule
       ],
+      providers: [{
+        provide: HttpClient, useValue: mockHttp
+      }],
       declarations: [
         Tag
       ],
@@ -20,5 +30,10 @@ describe(Tag.name, () => {
     const fixture = TestBed.createComponent(Tag);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+  });
+  it('should handle http.get', () => {
+    const fixture = TestBed.createComponent(Tag);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.data.setup).toEqual({ title: 'test', content: 'content\na' });
   });
 });
